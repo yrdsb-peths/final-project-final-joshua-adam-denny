cf16417dec480cee584b85abb21d8abfb84ba144
herrdorrsnorre <77855285+herrdorrsnorre@users.noreply.github.com>
2025-05-20 09:35:59 -0400

    d

--- MyWorld.java
+++ MyWorld.java
@@ -1,27 +1,21 @@
-import greenfoot.*;
-import java.util.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class MyWorld extends World {
-    private Player player;
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
 
-    public MyWorld() {
-        super(600, 400, 1, false); // disable auto repaint
-        player = new Player();
-        addObject(player, getWidth() / 2, getHeight() / 2); // Centered
-
-        // Add some objects to see scrolling
-        for (int i = 0; i < 10; i++) {
-            addObject(new Tree(), 100 + i * 100, 200); // Replace Tree with your Actor
-        }
-    }
-
-    // Move the world instead of the player
-    public void scrollWorld(int dx, int dy) {
-        List<Actor> all = getObjects(null);
-        for (Actor a : all) {
-            if (a != player) {
-                a.setLocation(a.getX() + dx, a.getY() + dy);
-            }
-        }
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
     }
 }
