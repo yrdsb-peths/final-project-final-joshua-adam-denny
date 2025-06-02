496c15816339d590f81eca9587017da89083b6a8
AdamFungus <afsomerset@gmail.com>
2025-06-02 11:05:39 -0400

    Sniper Ability
    hasnt had the damage multiplier applied yet due to some errors, after being clicked and going to next wave it seems a bit broken

--- Bullet.java
+++ Bullet.java
@@ -1,6 +1,8 @@
 public class Bullet extends Projectile {
     public Bullet(Enemy target, int damage, int speed) {
         super(target, damage, speed, "Bullet.png", 20); // 20 is radius
+        int actualDamage = damage;
+        
     }
 
 
